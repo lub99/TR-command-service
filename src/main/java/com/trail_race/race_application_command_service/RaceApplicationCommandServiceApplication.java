bcdf56ba8d0ac34d62356750b1a1ad22b3e71040
lubwizard <matej.luburic@stack-wizard.com>
2024-11-17 22:26:32 +0100

    Initial commit

---
+++ src/main/java/com/trail_race/race_application_command_service/RaceApplicationCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trail_race.race_application_command_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceApplicationCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceApplicationCommandServiceApplication.class, args);
+	}
+
+}
